f44e5b3d7cfc26f7ed8b3af965cd2bd6cc3ac4cd
maxwneto <maxwneto@gmail.com>
2020-05-16 01:46:05 -0300

    using cast

---
+++ chapter02/PromDemo.java
@@ -0,0 +1,18 @@
+/*
+   A promotion surprise!
+   @author Max Wilson
+   16 May 2020
+*/
+class PromDemo{
+   public static void main(String[] args){
+	byte b;
+	int i;
+
+	b = 10;
+	i = b * b;
+	b = (byte)  (b * b);
+
+	System.out.println("i and b: " + i + " " + b);
+
+   }
+}
