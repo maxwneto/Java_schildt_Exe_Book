3cb0e899046329aec15e9d7b603b11db8ad10a3f
maxwneto <maxwilson@localhost.localdomain>
2020-05-27 07:04:30 -0300

    studied constructor and this for initialize a object

---
+++ chapter04/ParmConsDemo.java
@@ -0,0 +1,20 @@
+/*
+	A parameterized constructor.
+	@author: Max Wilson
+	27 May 2020
+*/
+class MyClass{
+	int x;
+	
+	MyClass(int i){
+		x = i;
+	}
+}
+class ParmConsDemo{
+	public static void main (String[] args){
+		MyClass t1 = new MyClass(10);
+		MyClass t2 = new MyClass(88);
+		
+		System.out.println(t1.x + " " + t2.x);
+	}
+}
