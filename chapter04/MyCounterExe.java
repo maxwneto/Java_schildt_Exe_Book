134a49bb1daae496dce1fd33241b3276256ea8e6
maxwneto <maxwilson@localhost.localdomain>
2020-05-27 07:54:15 -0300

    exercises practicing class, method, this

---
+++ chapter04/MyCounterExe.java
@@ -0,0 +1,25 @@
+/*
+	counter object
+	@author: Max Wilson
+	27 May 2020
+*/
+class MyCounter{
+	int count;
+	
+	MyCounter(int count){
+		this. count = count;
+	}
+	
+	int getCount(){
+		return this.count;
+	}
+}
+
+class MyCounterExe{
+	public static void main(String[] args){
+		MyCounter counter = new MyCounter(10);
+		
+		System.out.println(counter.getCount());
+		
+	}
+}
