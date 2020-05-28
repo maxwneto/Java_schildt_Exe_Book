e51b4d9d1d6bdb29283d99c030d6c92f8009f4b7
maxwneto <maxwneto@gmail.com>
2020-05-28 17:36:22 -0300

    bubble sort

--- chapter05/MinMax2.java
+++ chapter05/MinMax2.java
@@ -1,24 +1,13 @@
 /*
-	Find the minimum and maximum values in an array.
+	Use Array initializers.
 	@author: Max Wilson
 	28 May 2020
 */
-class MinMax{
+class MinMax2{
 	public static void main(String[] args){
-		int nums[] = new int [10];
+		int nums[] = {99,-10,100123,18,-978,5623,463,-9,287,49};
 		int max, min;
-		
-		nums[0] = 99;
-		nums[1] = -10;
-		nums[2] = 100123;
-		nums[3] = 18;
-		nums[4] = -978;
-		nums[5] = 5623;
-		nums[6] = 463;
-		nums[7] = -9;
-		nums[8] = 287;
-		nums[9] = 49;
-		
+				
 		min = max = nums[0];
 		for (int i = 1;  i < 10; i++){
 			if(nums[i] < min) min = nums[i];
